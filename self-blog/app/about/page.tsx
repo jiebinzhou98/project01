20d72a9df9b9bb47e3153c82b243f9fde4eb7838
jiebinzhou98 <benzhou98@outlook.com>
2024-12-28 15:55:33 -0500

    Update about

--- self-blog/app/about/page.tsx
+++ self-blog/app/about/page.tsx
@@ -1,7 +1,15 @@
+"use client"
+import { useRouter } from "next/navigation";
+
 export default function About (){
+    const router = useRouter();
     return (
     <div>
         <h1>About page</h1>
+        <button onClick={() => router.push("/")}
+            className="bg-blue-500 text-white p2 rounded-md">
+                Go Back
+        </button>
     </div>
     )
 }
