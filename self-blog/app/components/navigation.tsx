03e9250c2a51e95f4bf9dd411ffd055a409379a7
jiebinzhou98 <benzhou98@outlook.com>
2024-12-28 15:18:20 -0500

    Update about

--- self-blog/app/components/navigation.tsx
+++ self-blog/app/components/navigation.tsx
@@ -3,6 +3,9 @@ import Link from "next/link";
 export const Navigation = () => {
     return (
         <nav className="flex justify-center items-center p-4">
+            <Link href={"/"} className="mr-4 text-blue-50">Home</Link>
+
+            <Link href={"/about"} className="mr-4 text-blue-50">About</Link>
         </nav>
       );
 }
--- self-blog/app/layout.tsx
+++ self-blog/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Navigation } from "./components/navigation";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -27,6 +28,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <header className="bg-slate-900 text-white p-4 text-center">
+          <Navigation/>
+        </header>
         {children}
       </body>
     </html>
