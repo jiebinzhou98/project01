c1b1ca32385e97ee90f5d9b48a58b42e2683b9a8
jiebinzhou98 <benzhou98@outlook.com>
2024-12-28 15:48:24 -0500

    Update about

--- self-blog/app/components/navigation.tsx
+++ self-blog/app/components/navigation.tsx
@@ -1,11 +1,17 @@
+"use client"
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export const Navigation = () => {
+    const pathname = usePathname();
+
     return (
         <nav className="flex justify-center items-center p-4">
-            <Link href={"/"} className="mr-4 text-blue-50">Home</Link>
+            <Link href={"/"} 
+            className={pathname === "/" ? "font-bold mr-4" : "text-blue-50 mr-4"}>Home</Link>
 
-            <Link href={"/about"} className="mr-4 text-blue-50">About</Link>
+            <Link href={"/about"} 
+            className={pathname === "/about" ? "font-bold mr-4" : "text-blue-50 mr-4"}>About</Link>
         </nav>
       );
 }
