c5f0a18b8a3397bd80259e29f618f92c541598f9
jiebinzhou98 <benzhou98@outlook.com>
2024-12-31 15:51:42 -0500

    Update footer

--- self-blog/app/components/footer.tsx
+++ self-blog/app/components/footer.tsx
@@ -1,8 +1,8 @@
-export default function Foooter() {
+export default function Footer() {
     return (
-      <footer className="bg-gray-800 text-white py-4 mt-10">
-        <div className="container mx-auto text-center">
-          <p className="text-sm">
+      <footer className="bg-gray-100 border-t border-gray-300 py-4 mt-10">
+        <div className="max-w-4xl mx-auto text-center">
+          <p className="text-sm text-black">
             &copy; {new Date().getFullYear()} Your Company. All Rights Reserved.
           </p>
         </div>
