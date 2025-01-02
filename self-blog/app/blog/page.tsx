55b52c5cb2bf24aff0ac1f29ef3eefae8e8ec277
jiebinzhou98 <benzhou98@outlook.com>
2025-01-02 14:49:37 -0500

    Update changes

--- self-blog/app/blog/page.tsx
+++ self-blog/app/blog/page.tsx
@@ -5,8 +5,18 @@ type Post = {
   body: string;
 };
 
-type BlogProps = {
-  posts: Post[];
-};
+export default async function Blog() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const datas: Post[] = await res.json(); // Explicitly typing the response
 
-
+  return (
+    <div className="grid grid-cols-4 gap-4 py-10">
+      {datas.map((data) => (
+        <div key={data.id} className="p-4 border rounded shadow-sm">
+          <h3 className="text-lg font-semibold">{data.title}</h3>
+          <p className="text-sm">{data.body}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
